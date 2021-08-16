d1cc8b0314abf09ed0b7798f107d50e88c76d87a
Renan <renan.supero@fornecedores.klabin.com.br>
2021-08-16 18:23:28 -0300

    Create Post component

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import Header from "./src/components/Header";
+import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <Header></Header>
+        <Post image={require("./assets/imgs/fence.jpg")}></Post>
       </SafeAreaView>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1
+  }
+})
---
+++ src/components/Post.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native'
+
+import Author from './Author'
+
+class Post extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image source={this.props.image}
+                    style={styles.image}
+                />
+                <Author email="renan@email.com" nickname="Renan"/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    image: {
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").width * 3 / 4,
+        resizeMode: 'contain'
+    }
+})
+
+export default Post;
