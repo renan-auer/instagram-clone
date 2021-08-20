818fad0c1c9cc4b83d28f9030874d4e832a93d12
Renan <renan.supero@fornecedores.klabin.com.br>
2021-08-20 01:36:09 -0300

    Use thunks middleware

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -1,10 +1,20 @@
 import { ADD_POST, ADD_COMMENT } from "./actionTypes"
+import axios from 'axios'
 
 export const addPost = post => {
+    return dispatch => {
+        axios.post('/posts.json', { ...post })
+            .then(res => {
+                console.warn(res)
+            })
+            .catch(err => console.warn(err))
+    }
+    /*
     return {
         type: ADD_POST,
         payload: post
     }
+    */
 }
 
 export const addComment = comment => {
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,4 +1,9 @@
-import { createStore, combineReducers } from 'redux'
+import {
+    createStore, combineReducers,
+    compose,
+    applyMiddleware
+} from 'redux'
+import thunk from 'redux-thunk'
 import userReducer from './reducers/user'
 import postReducer from './reducers/post'
 
@@ -8,7 +13,7 @@ const reducers = combineReducers({
 })
 
 const storeConfig = () => {
-    return createStore(reducers)
+    return createStore(reducers, compose(applyMiddleware(thunk)))
 }
 
 export default storeConfig
